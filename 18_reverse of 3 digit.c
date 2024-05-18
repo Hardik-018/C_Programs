eb4887b0002231d054f690a2011d70ea24157929
Hardik Pareek <hardikpareek018@gmail.com>
2024-05-18 18:57:44 +0530

    18-May-2024

--- 18_reverse of 3 digit.c
+++ 18_reverse of 3 digit.c
@@ -2,13 +2,13 @@
 #include<conio.h>
 int main(){
 	//n is a 3 dit number
-	int n,h,m,l,t;
+	int n,f,m,l,t,sum;
 	printf("Enter Three Digit Number:");
 	scanf("%d", &n);
-	
-	
-	
-	
-	
+	f=n/100; m=n/10%10; l=n%10;
+	t=f; f=l; l=t;
+	printf("Reverse is %d %d %d", f,m,l);
+	sum=f+m+l;
+	printf("\nSum is %d", sum);
 	return 0;
 }
