f4e8b00a9b18daa53e742b02d1cf621b5583bbb3
Hardik Pareek <hardikpareek018@gmail.com>
2024-05-29 21:48:59 +0530

    29-May-2024

---
+++ 37_Grades(else if).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	float p,Grade;
+	printf("Enter Percentage:");
+	scanf("%f", &p);
+	if(p>=90)
+		printf("Grade Is 'A'");
+	else if (p>=70)
+		printf("Grade Is 'B'");
+	else if (p>=50)
+		printf("Grade Is 'C'");
+	else if(p<50)
+		printf("Grade Is 'F'");
+return 0;
+}
